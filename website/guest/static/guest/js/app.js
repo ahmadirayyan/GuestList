7e482916435dd570550bff5e0ae4d2afc5991866
ahmadirayyan <ahmadirayyan@gmail.com>
2020-07-13 00:36:47 +0800

    remove static files

--- website/guest/static/guest/js/app.js
+++
@@ -1,9 +0,0 @@
-var s = $("#status").val();
-if (s=='success'){
-    $('#myModal').modal('show');
-}
-
-function sendData(id){
-    console.log(id);
-    $("#btn-delete").attr("href", "delete/"+id);
-}
